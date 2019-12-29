4006c7361736ac92d2424194291b907f6e34a028
Suban Hackit <37937252+hackit90@users.noreply.github.com>
2019-12-29 12:50:01 +0100

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,4 +1,15 @@
-//
-const val 
-const val 
+//main file for snack-o-mat
 
+//initiate input before main function
+//create constant non-mutable variable
+const val fach1 = "Mars"
+const val fach2 = "Snickers"
+
+//main function start
+fun main(){
+    val startAmount = 10
+    val kioskView = SnackKioskView()
+    val kiosk = Kiosk(kioskView, startAmount)
+    val kioskController = SnackKioskController(kiosk)
+    kioskView.setController(kioskController)
+    kioskView.showMenue(startAmount, startAmount)
